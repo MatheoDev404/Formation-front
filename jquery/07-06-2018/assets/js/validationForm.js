084c05777158a05758463ee38601a17dcd965dd4
MatheoDev404 <stunaultmatheo.dev@gmail.com>
2018-06-07 14:11:45 +0200

    exercice formulaire JS/jQuery 07-06-2018

---
+++ jquery/07-06-2018/assets/js/validationForm.js
@@ -0,0 +1,10 @@
+        /* -------------------------------------------------
+        |             ~~~   LA CONSIGNE     ~~~            |
+        |                                                  |
+        | Créez un Formulaire HTML Bootstrap avec les      |
+        | champs suivants :                                |
+        |                                                  |
+        |   1. Nom, Prénom, Email, Tel                     |
+        |   2. Réaliser la validation JS du Formulaire     |
+        |                                                  |
+        ------------------------------------------------- */
