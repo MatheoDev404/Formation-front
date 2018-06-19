091aca8edb6db4799e9f19df7d3f8b9ab9020260
MatheoDev404 <stunaultmatheo.dev@gmail.com>
2018-06-19 12:45:59 +0200

    add-task fini 19-06-2018

--- angular/TodoApp/src/app/add-task/add-task.component.ts
+++ angular/TodoApp/src/app/add-task/add-task.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Task } from '../shared/models/task';
 
 @Component({
   selector: 'app-add-task',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTaskComponent implements OnInit {
 
+  // cela me permet d'émettre un évenmement lors de la création d'une nouvelle tâche.
+  // @type {EventEmitter<any>}
+  @Output() newTaskEvent = new EventEmitter();
+
+  // la tâche a créé
+  // @type {Task}
+  task : Task = new Task();
+  
+  // Permet de réinitaliser le formulaire
+  // @type {boolean}
+  active = true;
+  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  // Cette fonction est appellée lors de la création d'une tâche.
+  addTask(){
+    // Lorsque l'utilisateru soumet sa tâche, j'émets l'évenement avec la nouvelle tâche.
+    this.newTaskEvent.emit({
+      task: this.task
+    });
+    this.task = new Task();
+    this.active = false;
+    
+    setTimeout(()=> this.active = true, 0);
+
   }
 
 }
--- angular/TodoApp/src/app/app.module.ts
+++ angular/TodoApp/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { AddTaskComponent } from './add-task/add-task.component';
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AddTaskComponent } from './add-task/add-task.component';
     AddTaskComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
