8cdc9ad9f66c79cd23b39d660754d68a938d34c1
MatheoDev404 <stunaultmatheo.dev@gmail.com>
2018-06-19 12:48:43 +0200

    fix error app.component 19-06-2018

--- angular/TodoApp/src/app/app.component.ts
+++ angular/TodoApp/src/app/app.component.ts
@@ -7,5 +7,14 @@ import { Task } from './shared/models/task';
   styleUrls: ['./app.component.css' ]
 })
 export class AppComponent {
+
   tasks: Task[];
+
+  // Cette fonction  se déclenche dans l'application lorsqu'une nouvelle tâche est créée par l'utilisateur dans le composant app-addTask.
+  // @param {Task}task
+  newTask(task: Task){
+    console.log(task);
+    //On ajoute ma nouvelle tâche dans le tableau de tâches
+    this.tasks.push(task)
+  }
 }
